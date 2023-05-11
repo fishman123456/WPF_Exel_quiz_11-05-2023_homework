3a58860191b92b15244339742ad0793a937f2ef9
fishman123456 <den9152@yandex.ru>
2023-05-11 22:12:10 +0700

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Exel_quiz_11_05_2023_homework
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
